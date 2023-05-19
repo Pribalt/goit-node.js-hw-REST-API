0cd3d11cf6a1fd9c671aad46b44a3f11854f5b47
Pribalt <vitalisvitalik@gmail.com>
2023-05-19 17:23:25 +0300

    fix db_host

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
-const { DB_HOST } = require("./config");
 
 const app = require("./app");
+
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Database connection successful");
     });
   })
