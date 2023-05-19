812083852e8b0f73cfe98baccb2ecac33b9b9782
Pribalt <vitalisvitalik@gmail.com>
2023-05-19 17:35:31 +0300

    fix1_routes_contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,32 @@
 const express = require("express");
-
 const {
   getContacts,
   getOneContact,
   addNewContact,
   deleteContact,
   updateContact,
+  updateFavoriteContact,
 } = require("../../controllers/contacts");
-
-const { validateBody } = require("..//..//middlewares");
-
-const { addShema } = require("..//..//shemas/contacts");
+const { validateBody, isValidId } = require("..//..//middlewares");
+const { schemas } = require("..//..//models/contact");
 
 const router = express.Router();
 
-router.route("/").get(getContacts).post(validateBody(addShema), addNewContact);
+router
+  .route("/")
+  .get(getContacts)
+  .post(validateBody(schemas.addSchema), addNewContact);
 router
   .route("/:contactId")
-  .get(getOneContact)
-  .delete(deleteContact)
-  .put(validateBody(addShema), updateContact);
+  .get(isValidId, getOneContact)
+  .delete(isValidId, deleteContact)
+  .put(isValidId, validateBody(schemas.addSchema), updateContact);
+router
+  .route("/:contactId/favorite")
+  .patch(
+    isValidId,
+    validateBody(schemas.updateFavoriteSchema),
+    updateFavoriteContact
+  );
 
 module.exports = router;
