f095fbd86b17a6010474ff2df42fcea424f1db56
Pribalt <vitalisvitalik@gmail.com>
2023-05-19 17:36:07 +0300

    fix1_utils_index.js

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,9 @@
 const asyncWrapper = require("./asyncWrapper");
 const HttpError = require("./HttpError");
+const handleMongooseError = require("./handleMongooseError");
 
 module.exports = {
   asyncWrapper,
   HttpError,
+  handleMongooseError,
 };
