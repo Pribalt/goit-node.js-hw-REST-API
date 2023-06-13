81bec6b82ce8f2c602bff5de8c63c9e234309f64
Pribalt <vitalisvitalik@gmail.com>
2023-06-13 12:00:08 +0300

    fix_contacts.js_controllers

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,8 +3,6 @@ const { Contact } = require("../models/contact");
 const { HttpError, asyncWrapper } = require("../utils");
 
 const getContacts = asyncWrapper(async (req, res) => {
-  const { _id: owner } = req.user;
-
   const { page = 1, limit = 10, favorite } = req.query;
 
   const skip = (page - 1) * limit;
